d3cd1690cc5dd43643acedd35fadd5bc6ea7b03a
DafyddEvans2813865 <156840512+DafyddEvans2813865@users.noreply.github.com>
2024-12-30 09:35:11 +0000

    init and hash-object implemented

--- ugit/cli.py
+++ ugit/cli.py
@@ -2,5 +2,42 @@ import argparse
 import os
 import sys
 
+from . import data
+
 def main ():
-    print ('Hello, World!')
+    args = parse_args()
+    args.function(args)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # Add a subparser group
+    commands = parser.add_subparsers(dest='command')# 'dest' specifies where the subcommand name is stored
+    commands.required = True
+
+    init_paraser = commands.add_parser('init')
+    init_paraser.set_defaults(function=init)
+
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(function=hash_object)
+    hash_object_parser.add_argument('file')
+
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(function=cat_file)
+    cat_file_parser.add_argument('object')
+
+    return parser.parse_args()
+
+def init(args):
+    data.init()
+    print (f'Initialized empty ugit repository in {os.getcwd()}\{data.GIT_DIR}')
+
+def hash_object(args):
+    with open(args.file,'rb') as f:
+        print(data.hash_object(f.read()))
+
+def cat_file(args):
+    # Flush stdout to ensure no buffered output remains
+    sys.stdout.flush()
+    # Write binary data of the specified object to stdout
+    sys.stdout.buffer.write (data.get_object(args.object))
