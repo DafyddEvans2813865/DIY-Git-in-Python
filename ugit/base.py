4be1f6274332c19b171bead12914ead7332ce06c
DafyddEvans2813865 <156840512+DafyddEvans2813865@users.noreply.github.com>
2025-01-13 09:32:10 +0000

    Hashed Objects can now be stored in a directory

--- ugit/base.py
+++ ugit/base.py
@@ -3,17 +3,25 @@ import os
 from . import data
 
 def write_tree(directory ='.'):
+    entries = []
     with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
             if is_ignored(full):
                 continue
             if entry.is_file(follow_symlinks=False):
-                #TODO write the file to object store
-                print(full)
+                type_ = 'blob'
+                with open(full, 'rb') as f:
+                    oid = data.hash_object(f.read())
             elif entry.is_dir(follow_symlinks=False):
-                write_tree(full)
-    #TODO return the oid of the tree
+                type_ = 'tree'
+                oid = write_tree(full)
+            entries.append((type_, entry.name, oid))
+    
+    tree = ''.join(f'{type_} {oid} {entry.name}\n' 
+                   for entry.name, oid,type_ 
+                   in sorted (entries))
+    return data.hash_object(tree.encode(), 'tree')
 
 def is_ignored(path):
     return '.ugit' in path.split('/')
